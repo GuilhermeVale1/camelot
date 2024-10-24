813cdb0b5d28f75d3c9b0a105c0e8ef4e390ef9c
Gui <valeguilherme01@gmail.com>
2024-10-23 23:57:54 -0300

    triangulação de vetor arrumada novamente

--- player.gd
+++ player.gd
@@ -37,6 +37,18 @@ func _physics_process(delta):
 		if( GameMananger.collectWeapon()):
 			animationMovment = "andandoMach"
 			animationStop = "paradoMach"
+	
+	var x_mov = Input.get_action_strength("direita") - Input.get_action_strength("esquerda")
+	# x_mov = 1 - 0 = 1 mov = right 
+	# x_mov = 0 - 1 = -1 mov = left 
+	
+	var y_mov = Input.get_action_strength("baixo") - Input.get_action_strength("cima")
+	# y_mov = 1 - 0 = 1 mov = down 
+	# y_mov = 0 - 1 = -1 mov = up
+	 
+	var mov = Vector2(x_mov, y_mov)
+			
+	velocity = mov.normalized()*speed
 	move_and_slide()
 
 	
