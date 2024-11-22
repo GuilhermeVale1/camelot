f0e7d0a9dbe48cdcffe9aa8b4640f536a5f8c7b3
Gui <valeguilherme01@gmail.com>
2024-11-22 18:13:44 -0300

    inimigo funcional

--- inimigo.gd
+++ inimigo.gd
@@ -4,23 +4,28 @@ extends CharacterBody2D
 
 var animationStop = "parado"
 var animationMovment = "andando"
+var speed: int 
+var player_chace = false
+var player = null
 
+
+	
 
 
 var nome : String
 var id : int
 
 var areaGolpe = false
-func _init(id : int , nome: String ):
+func _init(id : int , nome: String , speed:int):
 	self.id = id
 	self.nome = nome
+	self.speed = speed
 
 func _ready():
 	GameMananger.danoInimigo.connect(inimigoMorto)	
 
 func inimigoMorto():
-	print("cheguei aqui")
-	print(areaGolpe)
+	
 	if(areaGolpe):
 		queue_free()
 
@@ -30,7 +35,23 @@ func AreaGolpe(aGolpe):
 	
 
 
-
+func _physics_process(delta):
+	if player_chace:
+		position += (player.position - position)/ speed
+		$AnimatedSprite2D.play("andando")
+	else:
+		$AnimatedSprite2D.play("parado")
+	
 
 func _on_detection_area_body_entered(body):
-	pass # Replace with function body.
+		if(body.is_in_group("jogador")):
+			player_chace = true
+			player = body
+	
+
+
+func _on_detection_area_body_exited(body):
+	if(body.is_in_group("jogador")):
+			player_chace = false
+			player = null
+	print("saiu da zona inimiga")
